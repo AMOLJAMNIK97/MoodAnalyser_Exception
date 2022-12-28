84a1ffd373081238a71e6960c5440484e2e11d61
Amoljamnik97 <ajamnik.mech@gmail.com>
2022-12-28 23:18:59 +0530

    [amol]- Check mood happy or sad

---
+++ MoodAnalyserException/MoodAnalyserException/MoodAnalyser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserException
+{
+    public class MoodAnalyser
+    {
+        public string message;
+        //create parameterized constructor
+        public MoodAnalyser(string message) 
+        {
+            this.message = message;//assigning value to local variable
+        }
+        public string MoodAnalyzer()//create method to get mood using messege
+        {
+            if (message.ToLower().Contains("sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+
+    }
+}
---
+++ MoodAnalyserException/MoodAnalyserException/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Anlyser");
+            Console.WriteLine("Enter message ");
+            string message = Console.ReadLine();
+
+            MoodAnalyser obj = new MoodAnalyser(message);
+            string mood = obj.MoodAnalyzer();//calling method of class with passing user message as parameter
+            Console.WriteLine("Mood :{0}", mood);
+            Console.ReadLine();
+        }
+    }
+}
